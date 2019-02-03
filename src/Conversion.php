41aa704ed1eec78fda95ad6bfc2dfd9e201bb8e0
JuAlexandre <julien.alexandre26@gmail.com>
2019-02-03 15:25:26 +0100

    Clean code

--- src/Conversion.php
+++ src/Conversion.php
@@ -1,18 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wilder12
- * Date: 19/04/18
- * Time: 09:42
- */
 
 namespace Wcs;
-
 
 class Conversion
 {
     /**
-     * Convert binary to decimal
+     * Convert binary to decimal.
      * @param string $binary
      * @return int
      */
@@ -21,10 +14,11 @@ class Conversion
         $numbers = str_split($binary);
         $numbers = array_reverse($numbers);
         $result = 0;
+
         foreach ($numbers as $key => $number) {
             $result += $number * pow(2, $key);
         }
-        return $result;
 
+        return $result;
     }
 }
--- tests/ConversionTest.php
+++ tests/ConversionTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wilder12
- * Date: 19/04/18
- * Time: 09:40
- */
 
 use PHPUnit\Framework\TestCase;
 use Wcs\Conversion;
